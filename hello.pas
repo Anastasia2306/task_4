d74cf98a1ce2f2d39c83a1943480a08e8c03b96f
Анастасия Переверзева <anastasia@1c-developer.ru>
2024-10-13 12:56:49 +0300

    Initial commit

---
+++ hello.pas
@@ -0,0 +1,11 @@
+﻿begin
+  var n := ReadInteger('Выберите язык: 1 - русский язык, 2 - английский язык, 3 - французский язык --- ');
+  Assert(n in 1..3);
+  if n = 1 then Writeln($'Привет, мир!')
+  else
+  begin
+    if n = 2 then Writeln($'Hello, world!')
+    else
+      Writeln($'Bonjour le monde!')
+  end;
+end.
